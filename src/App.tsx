c0f38413b21145f50f2b29fdbafc85b2a44f843c
Islam Temirgaliev <islam2004tr@mail.ru>
2023-11-30 13:23:49 +0600

    c

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,8 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-
-  const hendler  = () => {
-    
-  } 
+ 
+  
   return (
     <>
     <div className='conteiner'>
