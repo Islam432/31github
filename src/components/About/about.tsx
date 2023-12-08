000e2f8fa53cf255ebd6452c325a6e7b0464cad4
Islam Temirgaliev <islam2004tr@mail.ru>
2023-12-08 23:26:46 +0600

    ee

--- src/components/About/about.tsx
+++ src/components/About/about.tsx
@@ -2,7 +2,10 @@ import styels from './about.module.css'
 const about = () => {
   return (
     <div className={styels.conteiner}>
-
+     <div>
+      hello
+      p
+     </div>
     </div>
   )
 }
