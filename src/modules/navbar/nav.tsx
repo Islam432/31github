b0239fc6544357d12c611f4f16b6a93c4332dca8
Islam Temirgaliev <islam2004tr@mail.ru>
2023-12-09 22:46:29 +0600

    ,jn

--- src/modules/navbar/nav.tsx
+++ src/modules/navbar/nav.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import styles from "./nav.module.css"
 const nav = () => {
   return (
-    <div className={styles.conteiner}>nav</div>
+    <div className={styles.conteiner}>
+      <p>jhjk</p>
+    </div>
   )
 }
 
