8a6edb418abda705174003e7be5dd261721f1df0
Islam Temirgaliev <islam2004tr@mail.ru>
2023-12-13 22:30:55 +0600

    a

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const layout = (children: any) => {
   return (
-    <div>layout</div>
+    <div>
+      
+      <main></main>
+    </div>
   )
 }
 
