d28fb5594853dea0ad2b48049e684480be4188c7
Islam Temirgaliev <islam2004tr@mail.ru>
2023-12-14 20:56:43 +0600

    daw

--- src/modules/navbar/nav.tsx
+++ src/modules/navbar/nav.tsx
@@ -3,7 +3,7 @@ import styles from "./nav.module.css"
 const nav = () => {
   return (
     <div className={styles.conteiner}>
-      <p className='hola'>\<div className='hend'>
+      <p className='hola'>\<div className='hid'>
         hola</div></p>
     </div>
   )
