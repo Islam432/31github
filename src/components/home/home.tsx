c0dfd1abf733f726475989de3b6e4e27e5dec1a8
Islam Temirgaliev <islam2004tr@mail.ru>
2023-12-18 22:34:20 +0600

    aww

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const home = () => {
   return (
     <div className='conteiner'>
-        <div>
+        <div className='power'>
             <p>helo </p>
         </div>
     </div>
